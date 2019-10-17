8ff31a9871ea0338f3f0199c608719a0b26c4950
Ben Godfrey <BenJetson@users.noreply.github.com>
2019-10-17 13:21:02 -0400

    Upload starter code.

---
+++ alexa.py
@@ -0,0 +1,119 @@
+import json
+from dataclasses import dataclass, field
+from collections import defaultdict
+from typing import Any, Callable, Dict
+
+@dataclass
+class Request:
+    """Request provides useful methods for getting data out of Alexa requests."""
+    event: dict
+
+    TYPE_INTENT = "IntentRequest"
+    TYPE_LAUNCH = "LaunchRequest"
+    TYPE_HELP = "HelpRequest"
+
+    def request_type(self) -> str:
+        return self.event["request"]["type"]
+
+    def read_session_storage(self) -> Dict[Any, Any]:
+        return self.event["sessionAttributes"]
+
+
+@dataclass
+class IntentRequest(Request):
+    """IntentRequest provides useful methods for handling intent requests."""
+
+    def intent_name(self) -> str:
+        return self.event["request"]["intent"]["name"]
+
+    def value_of_slot(self, slot_name) -> str:
+        if slot_name in self.event["request"]["intent"]["slots"]:
+            return self.event["request"]["intent"]["slots"][slot_name]["value"]
+        return None
+
+    def id_value_of_slot(self, slot_name) -> str:
+        if slot_name not in self.event["request"]["intent"]["slots"]:
+             return None
+        
+        slot = self.event["request"]["intent"]["slots"][slot_name]
+        if "resolutions" in slot and "resolutionsPerAuthority" in slot["resolutions"]:
+            all_resolutions = slot["resolutions"]["resolutionsPerAuthority"]
+
+            for resolution in all_resolutions:
+                for value in resolution["values"]:
+                    value = value["value"]
+                    if "id" in value:
+                        return value["id"]
+
+        return None
+
+
+@dataclass
+class Card:
+    """Card holds data that will be presented to the user as a card via the Alexa app."""
+    title: str = ""
+    body: str = ""
+    image_url: str = ""
+
+
+@dataclass
+class Response:
+    """Response provides an easy interface for creating responses to Alexa requests."""
+    speech: str = ""
+    card: Card = None
+    session_storage: Dict[Any, Any] = field(default_factory=defaultdict)
+    end_session: bool = True
+
+    def to_dict(self):
+        output = {
+            "version": "1.0",
+            "sessionAttributes": self.session_storage,
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": self.speech
+                },
+                "shouldEndSession": self.end_session
+            }
+        }
+
+        if self.card is not None:
+            output["card"] = {
+                "type": "Standard",
+                "title": self.card.title,
+                "text": self.card.body,
+                "image": {
+                    "smallImageUrl": self.card.image_url,
+                    "largeImageUrl": self.card.image_url
+                }
+            }
+        
+        return output
+
+
+@dataclass
+class Router:
+    """Router helps route an Alexa request to the appropriate response generator."""
+
+    request_handlers: Dict[str, Callable[[Request], Response]]
+    intent_handlers: Dict[str, Callable[[IntentRequest], Response]]
+
+    def handle_request(self, event, context) -> dict:
+        request = Request(event)
+        request_type = request.request_type()
+        response = Response()
+
+        if request_type == Request.TYPE_INTENT:
+            intent_request = IntentRequest(request.event)
+            intent_name = intent_request.intent_name()
+
+            if intent_name in self.intent_handlers:
+                handler = self.intent_handlers[intent_name]
+                response = handler(intent_request)
+
+        elif request_type in self.request_handlers:
+            handler = self.request_handlers[request_type]
+            response = handler(request)
+
+        return response.to_dict()
+        
---
+++ hello_world.py
@@ -0,0 +1,93 @@
+import alexa
+import random
+
+sample_questions = [
+    "When is the hackathon?",
+    "What time is cup stacking?",
+    "What time is judging?"
+]
+
+hackathon_events = {
+    "OPEN_CEREMONY": 
+        ("8:30 am", "Opening Ceremony"),
+    "BEGIN_HACK": 
+        ("9:00 am", "Hacking Begins"),
+    "IDEA_JAM": 
+        ("9:30 am", "Idea Jam"),
+    "GITHUB_WORKSHOP": 
+        ("10:30 am", "Intro to GitHub Workshop"),
+    "LUNCH": 
+        ("12:00 pm", "Lunch"),
+    "CUP_STACK": 
+        ("2:00 pm", "Cup Stacking"),
+    "VR_DEMO": 
+        ("3:30 pm", "VR Demo"),
+    "END_HACK": 
+        ("6:00 pm", "Hacking Ends"),
+    "DINNER": 
+        ("6:30 pm", "Dinner"),
+    "JUDGE": 
+        ("7:00 pm", "Judging and Demos"),
+    "CLOSE_CEREMONY": 
+        ("8:00 pm", "Closing Ceremony")
+}
+
+
+def handle_request_launch(request: alexa.Request) -> alexa.Response:
+    response = alexa.Response()
+
+    response.speech = "Hi there! You've successfully launched the Hello World demo skill. "
+    response.speech += "You can ask me basic questions about the hackathon. What do you want to know?"
+
+    response.end_session = False
+
+    return response
+
+
+def handle_request_help(request: alexa.Request) -> alexa.Response:
+    response = alexa.Response()
+
+    response.speech = "You can ask me basic questions about the hackathon. For example, try asking: "
+    response.speech += random.choice(sample_questions)
+
+    return response
+
+
+def handle_intent_what_day(request: alexa.Request) -> alexa.Response:
+    response = alexa.Response()
+
+    response.speech = "CU hack-it Hello World 2019 will take place on Saturday, October 19th, 2019."
+
+    return response
+
+
+def handle_intent_event_time(request: alexa.IntentRequest) -> alexa.Response:
+    response = alexa.Response()
+
+    event_id = request.id_value_of_slot("event_name")
+
+    if event_id is None or event_id not in hackathon_events:
+        response.speech = "Sorry, I don't have information about that event."
+        return response
+    
+    event_details = hackathon_events[event_id]
+    event_time = event_details[0]
+    event_name = event_details[1]
+
+    response.speech = "{} is at {}.".format(event_name, event_time)
+
+    return response
+
+
+request_handlers = {
+    alexa.Request.TYPE_LAUNCH: handle_request_launch,
+    alexa.Request.TYPE_HELP: handle_request_help
+}
+
+intent_handlers = {
+    "what_day": handle_intent_what_day,
+    "event_time": handle_intent_event_time
+}
+
+router = alexa.Router(request_handlers, intent_handlers)
+lambda_handler = router.handle_request
