efbc0ba77938c44c16ef6b6a86bec060ead47ca6
Ben Godfrey <BenJetson@users.noreply.github.com>
2019-10-17 23:54:49 -0400

    Add license notices to the top of all source files.

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,20 @@
+# Hello World 2019 Alexa Skill
+# Provides information about the CUhackit Hello World '19 hackathon event.
+# Copyright (C) 2019 Ben Godfrey
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import alexa
 import random
 import logging
